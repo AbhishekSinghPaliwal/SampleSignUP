9cd2eac6ae579ffc4683a508d92325ece243feeb
AbhishekSinghPaliwal <abhi.myth21@gmail.com>
2018-10-17 18:22:39 +0530

    ElementActions.java

--- src/main/java/com/cognitivescale/framework/Utilities/ElementActions.java
+++ src/main/java/com/cognitivescale/framework/Utilities/ElementActions.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -17,60 +18,61 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.cognitivescale.framework.common.Base;
 
-public class ElementActions extends Base{
-	
-	//public WebDriver driver;
-	public static int count=1;
+public class ElementActions extends Base {
+
+	// public WebDriver driver;
+	public static int count = 1;
 	public WebDriverWait wait;
 	private static final int GLOBAL_WAIT = 90;
-	
-	
-	/*public ElementActions(){
-		driver=getDriver();
-	}*/
 
-	//FacebookSignUpPage facebookSignUpPage;
-	public void inputData(WebElement element,WebDriver driver,String firstName){
-		
-		waitForElementVisibility(element,driver);
-		
-		
+	/*
+	 * public ElementActions(){ driver=getDriver(); }
+	 */
+
+	// FacebookSignUpPage facebookSignUpPage;
+	public void inputData(WebElement element, WebDriver driver, String firstName) {
+
+		waitForElementVisibility(driver, element);
+
 		element.sendKeys(firstName);
-		
-		
+
 	}
-	
-	public void waitForElementVisibility(WebElement element,WebDriver driver){
-		wait=new WebDriverWait(driver, GLOBAL_WAIT);
+
+	public void waitForElementClicable(WebDriver driver, WebElement element) {
+		wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+
+	}
+
+	public void waitForElementVisibility(WebDriver driver, WebElement element) {
+		wait = new WebDriverWait(driver, GLOBAL_WAIT);
 		wait.until(ExpectedConditions.visibilityOf(element));
 		System.out.println("end of  waitForElementVisibility ");
-		
-		
-		
+
 	}
-	
-	public void drodownValueSelection(WebElement element,String value){
+
+	public void jsExecutor(WebElement element, WebDriver driver) {
+		waitForElementClicable(driver, element);
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeScript("arguments[0].click", element);
+
+	}
+
+	public void drodownValueSelection(WebElement element, String value) {
 		new Select(element).selectByValue(value);
 	}
-	
-	/*public void waitForPageLoad(WebDriver driverObj){
-		driverObj.manage().timeouts().implicitlyWait(GLOBAL_WAIT, TimeUnit.SECONDS);
-		
-		
-		
-		
-	}*/
-	
-	public void screenshotcaptureAndSave(String filename,WebDriver driver) throws IOException{
-		count=count+1;
-		Date date=new Date();
-		TakesScreenshot screenshot=(TakesScreenshot)driver;
-		File sourceFile=screenshot.getScreenshotAs(OutputType.FILE);
-//		String path="D:\\EclipseWorkSpaceNew\\com.cognitivescale.test\\Screenshots\\"+filename+"+date+"+".png";
-		String path="D:\\EclipseWorkSpaceNew\\com.cognitivescale.test\\Screenshots\\"+filename+".png";
-		File destinationPath=new File(path);
+
+	public void screenshotcaptureAndSave(String filename, WebDriver driver) throws IOException {
+		count = count + 1;
+		Date date = new Date();
+		TakesScreenshot screenshot = (TakesScreenshot) driver;
+		File sourceFile = screenshot.getScreenshotAs(OutputType.FILE);
+		// String
+		// path="D:\\EclipseWorkSpaceNew\\com.cognitivescale.test\\Screenshots\\"+filename+"+date+"+".png";
+		String path = "D:\\EclipseWorkSpaceNew\\com.cognitivescale.test\\Screenshots\\" + filename + ".png";
+		File destinationPath = new File(path);
 		FileUtils.copyFile(sourceFile, destinationPath);
-		
+
 	}
 
 }
