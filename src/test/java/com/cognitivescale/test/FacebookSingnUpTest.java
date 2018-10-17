d8dd07c07955b5e347142a469c8e7323c2a02992
AbhishekSinghPaliwal <abhi.myth21@gmail.com>
2018-10-17 19:30:56 +0530

    .

--- src/test/java/com/cognitivescale/test/FacebookSingnUpTest.java
+++ src/test/java/com/cognitivescale/test/FacebookSingnUpTest.java
@@ -1,7 +1,5 @@
 package com.cognitivescale.test;
 
-
-
 import java.io.IOException;
 
 import org.apache.commons.lang3.RandomStringUtils;
@@ -13,40 +11,37 @@ import com.cognitivescale.framework.Utilities.ElementActions;
 import com.cognitivescale.framework.businessLogic.FacebookSignUpPage;
 import com.cognitivescale.framework.common.Base;
 
-
-
-public class FacebookSingnUpTest extends Base{
+public class FacebookSingnUpTest extends Base {
 	private static FacebookSignUpPage facebookSignUpPage;
-	ElementActions elementActions=new ElementActions();
-	WebDriver driver;
-	//public FacebookSignUpPage facebookSignUpPage;
-	//Base base=new Base();
-	
+	ElementActions elementActions = new ElementActions();
+
 	@BeforeClass
-	public void launchApplication(){
+	public void launchApplication() {
 		System.out.println("inside launchApplication");
-		//driver=getDriver();
-		facebookSignUpPage=new FacebookSignUpPage();
-		
+
+		facebookSignUpPage = new FacebookSignUpPage();
+
 	}
-	
+
 	@Test
-	public void facebookSignUPForm() throws InterruptedException, IOException{
-		//String randomName=RandomStringUtils.randomAlphabetic(8);
-		facebookSignUpPage.setFirstName(getConfig("firstName"));
-		facebookSignUpPage.setSurName(getConfig("surName"));
-		facebookSignUpPage.setUserEmail(getConfig("userEmail"));
-		facebookSignUpPage.setUserEmailConfirmation(getConfig("userEmailConfirmation"));
-		
-		facebookSignUpPage.setPassword(getConfig("password"));
-		facebookSignUpPage.setBirthdayDate(getConfig("birthdayDate"));
-		facebookSignUpPage.setBirthdayMonth(getConfig("birthdayMonth"));
-		facebookSignUpPage.setBirthdayYear(getConfig("birthdayYear"));
-		
-		
-		
+	public void facebookSignUPForm() throws InterruptedException, IOException {
+		try {
+			// String randomName=RandomStringUtils.randomAlphabetic(8);
+			facebookSignUpPage.setFirstName(getConfig("firstName"));
+			facebookSignUpPage.setSurName(getConfig("surName"));
+			facebookSignUpPage.setUserEmail(getConfig("userEmail"));
+			facebookSignUpPage.setUserEmailConfirmation(getConfig("userEmailConfirmation"));
+
+			facebookSignUpPage.setPassword(getConfig("password"));
+			facebookSignUpPage.setBirthdayDate(getConfig("birthdayDate"));
+			facebookSignUpPage.setBirthdayMonth(getConfig("birthdayMonth"));
+			facebookSignUpPage.setBirthdayYear(getConfig("birthdayYear"));
+			facebookSignUpPage.setGenderRadioButton();
+
+		} catch (Exception exe) {
+			System.out.println("Execption details are  " + exe);
+		}
+
 	}
-	
-	
 
 }
